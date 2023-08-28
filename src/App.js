92300c694d36ffd95097e9e22817d46ec9ea2197
Estefania Rizzo <tefa49395@gmail.com>
2023-08-28 17:37:23 -0500

    enrutamiento cine alternativo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-
 import './App.css';
 import Home from './Components/Home/Index';
-import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import CineAlternativo from './Components/Pages/CineAlternativo';
+
 function App() {
   return (
     <Router>
-   
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      
-    </Routes>
-    
-  </Router>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path="/cine-alternativo" element={<CineAlternativo />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,14 +1,14 @@
 import Nav from 'react-bootstrap/Nav';
-
+import { Link } from 'react-router-dom';
 import './index.css';
 
 function NavBar() {
   return (
     <nav className='Nav-Item'>
        <Nav.Link className='Nav-Link' href='/'>Inicio</Nav.Link>
-      <Nav.Link className='Nav-Link' href='/home'>En Cartelera</Nav.Link>
-      <Nav.Link className='Nav-Link' eventKey='link-1'>Proximos Estrenos</Nav.Link>
-      <Nav.Link className='Nav-Link' eventKey='link-2'>Cine Alternativo</Nav.Link>
+      <Nav.Link className='Nav-Link' href='*'>En Cartelera</Nav.Link>
+      <Nav.Link className='Nav-Link' href='*'>Proximos Estrenos</Nav.Link>
+      <Nav.Link className='Nav-Link'href='cine-alternativo'>Cine Alternativo</Nav.Link>
     </nav>
     
   );
