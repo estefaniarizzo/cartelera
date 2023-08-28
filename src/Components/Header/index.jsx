b10e39e0e6adccfe9a3f5c7ffce88f18aad59c76
Estefania Rizzo <tefa49395@gmail.com>
2023-08-28 14:36:42 -0500

    nuevos commits

---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,17 @@
+import Nav from 'react-bootstrap/Nav';
+
+import './index.css';
+
+function NavBar() {
+  return (
+    <nav className='Nav-Item'>
+      
+      <Nav.Link className='Nav-Link' href='/home'>En Cartelera</Nav.Link>
+      <Nav.Link className='Nav-Link' eventKey='link-1'>Proximos Estrenos</Nav.Link>
+      <Nav.Link className='Nav-Link' eventKey='link-2'>Cine Alternativo</Nav.Link>
+    </nav>
+    
+  );
+}
+
+export default NavBar;
---
+++ src/Components/Home/Index.jsx
@@ -0,0 +1,10 @@
+import NavBar from "../Header";
+import CarrouselCartelera from "../Carrousel-Cartelera";
+const Home =()=>{
+    return(
+        <><NavBar />
+        <CarrouselCartelera /></>  
+    )
+}
+
+export default Home;
