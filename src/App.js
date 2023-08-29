2d5365e94622c02668bab04b7c3f65308c60fb31
Estefania Rizzo <tefa49395@gmail.com>
2023-08-29 10:58:19 -0500

    error 404

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CineAlternativo from './Components/Pages/CineAlternativo';
 import EnCartelera from './Components/Pages/EnCartelera';
 import ProximosEstrenos from './Components/Pages/ProximosEstrenos';
+import Page_error404 from './Components/Error404';
 function App() {
   return (
     <Router>
@@ -12,6 +13,7 @@ function App() {
         <Route path="/cine-alternativo" element={<CineAlternativo />} />
         <Route path="/en-cartelera" element={<EnCartelera />} />
         <Route path="/proximos-estrenos" element={<ProximosEstrenos />} />
+        <Route path='*' element={<Page_error404/>}/>
       </Routes>
     </Router>
   );
---
+++ src/Components/Error404/index.jsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom'
+import './index.css'
+const Page_error404 = () => {
+    return (
+        <div>
+            
+            <section className='div-error'>
+                
+                <Link className='enlace-volver' to="/">Volver</Link>
+            </section>
+            
+        </div>
+    )
+}
+export default Page_error404
